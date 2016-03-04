62d56a7bb8b62e9145e3e474a23df1f67fa704e1
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-03-04 11:14:25 +0100

    added GEN Labo1 Tests

---
+++ GEN/Labo2/src/AnnuaireTest.java
@@ -0,0 +1,71 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnnuaireTest {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testGetPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testAddPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testDelPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testChangeNomPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testChangePrenomPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testChangeAnneePersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testGetAnneePersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testGetNomPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testGetPrenomPersonne() throws Exception {
+
+   }
+
+   @Test
+   public void testExist() throws Exception {
+
+   }
+
+   @Test
+   public void testGetNombrePersonnes() throws Exception {
+
+   }
+}
---
+++ GEN/Labo2/src/PersonneTest.java
@@ -0,0 +1,58 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonneTest {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testGetNom() throws Exception {
+
+   }
+
+   @Test
+   public void testGetPrenom() throws Exception {
+
+   }
+
+   @Test
+   public void testGetEmail() throws Exception {
+
+   }
+
+   @Test
+   public void testGetAnnee() throws Exception {
+
+   }
+
+   @Test
+   public void testSetNom() throws Exception {
+
+   }
+
+   @Test
+   public void testSetPrenom() throws Exception {
+
+   }
+
+   @Test
+   public void testSetEmail() throws Exception {
+
+   }
+
+   @Test
+   public void testSetAnnee() throws Exception {
+
+   }
+}
