449b7346a0c4db1a0e19d59e331c5d2dcb4f6cda
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-03-22 10:40:24 +0100

    Quelques modif pour MCR

--- MCR/Labo2/src/Singleton.java
+++ MCR/Labo2/src/Singleton.java
@@ -1,4 +1,6 @@
-public class Singleton {
+import java.awt.*;
+
+public class Singleton implements Displayer {
    private static Singleton instance;
    private int data;  // état du singleton
 
@@ -15,5 +17,30 @@ public class Singleton {
 
    public void setData(int data) {
 	  this.data = data;
+   }
+
+   @Override
+   public int getWidth() {
+	  return 0;
+   }
+
+   @Override
+   public int getHeight() {
+	  return 0;
+   }
+
+   @Override
+   public Graphics2D getGraphics() {
+	  return null;
+   }
+
+   @Override
+   public void repaint() {
+
+   }
+
+   @Override
+   public void setTitle(String s) {
+
    }
 }
