a8e15594b550d7cc93dbb0267e35426f85a7a35f
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-06 22:43:08 +0200

    [MCR - Labo3]
    [MCR - Labo3] Fix bug when playing twice in a row

--- PCO/Labo3/ressources/bmmanager.cpp
+++ PCO/Labo3/ressources/bmmanager.cpp
@@ -8,18 +8,19 @@
 // En microsecondes
 #define DelaiLocal  4000000
 
+#define NbrDeRouleaux 3
 class RouleauThread : public QThread
 {
 private:
-    static int nb;
-    int pos = nb++;
+    size_t pos;
 public:
 
+    RouleauThread(size_t pos) : pos(pos){}
+    int valeur;
     void run() Q_DECL_OVERRIDE {
-        int valeur;
         valeur = 0;
         setValeurRouleau(pos,valeur);
-        for(;;){
+        while(!isInterruptionRequested()){
             usleep(10000);
             valeur = (valeur + 1) % 10;
             setValeurRouleau(pos,valeur);
@@ -27,33 +28,55 @@ public:
     }
 };
 
-RouleauThread rouleaux[3];
+class DelaiThread : public QThread
+{
+public:
+    void run() Q_DECL_OVERRIDE {
+        while(true){
+
+        }
+    }
+};
+
+RouleauThread* rouleaux[NbrDeRouleaux];
 int current = 0;
 int jackpot = 0;
 
 void BmManager::start()
 {
+
 }
 
 void BmManager::end()
 {
-    for(RouleauThread& rt : rouleaux)
-        rt.terminate();
+    for(RouleauThread* rt : rouleaux)
+        rt->terminate();
 }
 
 void BmManager::pieceIntroduite()
 {
     setJackpot(++jackpot);
-    for(RouleauThread& rt : rouleaux)
-        rt.start();
+
+    for(int i = 0; i < NbrDeRouleaux; ++i){
+        rouleaux[i] = new RouleauThread(i);
+        rouleaux[i]->start();
+    }
 }
 
 void BmManager::boutonStop()
 {
-    rouleaux[current++].terminate();
-    if(current == 2){ // Les 3 sont stoppés
+    rouleaux[current++]->requestInterruption();
+    if(current == NbrDeRouleaux){ // Les 3 sont stoppés
+
+        if((rouleaux[0]->valeur == rouleaux[1]->valeur) == rouleaux[2]->valeur){ // Jackpot
+
+        }
+
+        for(int i = 0; i < NbrDeRouleaux; ++i){
+            rouleaux[i]->wait();
+            delete rouleaux[i];
+        }
+        current = 0;
 
     }
 }
-
-int RouleauThread::nb = 0;
