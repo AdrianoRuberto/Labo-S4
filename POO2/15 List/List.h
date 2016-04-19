14d2ecfa335179b4ab1780e5df80ff98da02061b
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-19 11:12:31 +0200

    [POO2 - Labo15] - Coded functions for main to work

--- POO2/15 List/List.h
+++ POO2/15 List/List.h
@@ -5,90 +5,149 @@
 #ifndef LIST_LIST_H
 #define LIST_LIST_H
 
+#include <stdexcept>
+
+using namespace std;
+
 template<typename T>
 class List {
 
 private:
-
 	class Element {
-		Element* next;
-		Element* before;
+		friend ostream& operator<<(ostream& os, const Element& e) {
+			return os << e.data;
+		}
+	private:
 		T data;
 
+	public:
 		Element(T data, Element* next = nullptr, Element* before = nullptr) : next(next), before(before), data(data) {
-
 		}
+
+		Element* next;
+		Element* before;
 	};
 
-	Element* head;
-	Element* tail;
+	Element* head = nullptr;
+	Element* tail = nullptr;
 
 public:
 	class Iterator {
 	private:
 		Element* curr;
 	public:
-		Iterator& operator++(){ // prefix ++
-
+		Iterator(Element* elem){
+			curr = elem;
+		}
+		Iterator& operator++() { // prefix ++
+			curr = curr->next;
 			return *this;
 		}
-		Iterator operator++(int){ //suffix ++
 
+		Iterator operator++(int) { //suffix ++
+			Iterator tmp = *this;
+			curr = curr->next;
+			return tmp;
 		}
+
 		Iterator& operator--() { // prefix --
-
+			curr = curr->before;
+			return *this;
 		}
-		Iterator operator--(int){ // suffix --
 
+		Iterator operator--(int) { // suffix --
+			Iterator tmp = *this;
+			curr = curr->before;
+			return tmp;
+		}
+
+		Element& operator*() {
+			return *curr;
 		}
 
 		bool operator==(const Iterator& it) const {
-			if(curr == it.curr){
-
-			}
+			return curr == it.curr;
 		}
 
-		bool operator!=() const {
-
+		bool operator!=(const Iterator& it) const {
+			return !(*this == it);
 		}
+
+
 	};
 
 public:
-	List<T>() {
-		head = tail = nullptr;
-	}
+	List<T>() { }
 
 	List<T>(const List<T>& other) {
 
 	}
 
 	List<T>& operator=(const List<T>& other) {
+		if (this != &other) {
+			clear();
 
+			for (Iterator it = other.begin(); it != end(); ++it) // Copie les éléments
+				insert(*it);
+		}
 		return *this;
 	}
 
-	Element* operator[](int i) {
+	Element* operator[](const size_t index) {
+		if (index < 0 || index >= size()) throw out_of_range("index is out of bounds : " + index);
 
+		size_t i = index;
+		Iterator it = begin();
+		while (i-- > 0)
+			++it;
+
+		return &*it;
 	}
 
 	size_t size() const {
-
+		size_t s = 0;
+		for (Iterator it = begin(); it != end(); ++s, ++it);
+		return s;
 	}
 
 	void insert(T o) {
-
+		if (head == nullptr) {
+			head = tail = new Element(o);
+		} else {
+			head = new Element(o, head);
+			head->next->before = head;
+		}
 	}
 
 	void append(T o) {
+		if (head == nullptr) {
+			head = tail = new Element(o);
+		} else {
+			tail = new Element(o, nullptr, tail);
+			tail->before->next = tail;
+		}
+	}
+
+	void remove(const size_t index) {
+		Element* curr = (*this)[index];
+		if (curr->before) curr->before->next = curr->next;
+		if (curr->next) curr->next->before = curr->before;
+
+		if (curr == head) head = curr->next;
+		if (curr == tail) tail = curr->before;
+		delete curr;
+	}
+
+	void remove(T o) {
 
 	}
 
-	Iterator begin() {
-
+	Iterator begin() const {
+		return Iterator(head);
 	}
 
-	Iterator end() {
-
+	Iterator end() const {
+		return Iterator(tail->next);
 	}
 
 	Iterator find(T o) const {
@@ -96,7 +155,12 @@ public:
 	}
 
 	~List() {
+		clear();
+	}
 
+private:
+	void clear() {
+		while (head != nullptr) remove(0);
 	}
 
 };
--- POO2/15 List/main.cpp
+++ POO2/15 List/main.cpp
@@ -6,15 +6,21 @@ using namespace std;
 
 int main(){
 
-	List<string> l;
-	l.append("un");
-	l.append("deux");
-	l.append("trois");
-	/*
-	for (List<string>::Iterator it = l.begin(); it != l.end(); it++)
+	List<string> strings;
+	strings.append("un");
+	strings.append("deux");
+	strings.append("trois");
+
+	for (List<string>::Iterator it = strings.begin(); it != strings.end(); it++)
 		cout << *it << " ";
 	cout << endl;
-	 */
+	List<int> ints;
+	ints.insert(3);
+	ints.insert(2);
+	ints.insert(1);
+	for (List<int>::Iterator it = ints.begin(); it != ints.end(); it++)
+		cout << *it << " ";
+	cout << endl;
 
 	return EXIT_SUCCESS;
 }
