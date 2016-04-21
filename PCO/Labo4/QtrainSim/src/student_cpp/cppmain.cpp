e095238524c1833a3733d86e07e82e5eb27ea241
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-21 17:37:08 +0200

    Added poo2 and modify pco

--- PCO/Labo4/QtrainSim/src/student_cpp/cppmain.cpp
+++ PCO/Labo4/QtrainSim/src/student_cpp/cppmain.cpp
@@ -4,12 +4,14 @@
 #include <QList>
 
 //Creation d'une locomotive
-static Locomotive locomotive;
+static Locomotive rose;
+static Locomotive jaune;
 
 //Arret d'urgence
 void emergency_stop()
 {
-    locomotive.arreter();
+    rose.arreter();
+    jaune.arreter();
     afficher_message("\nSTOP!");
 }
 
@@ -23,36 +25,44 @@ int cmain()
 
     //Initialisation d'un parcours
     QList<int> parcours;
-    parcours << 7 << 15 << 14 << 7 << 6 << 5 << 34 << 33 << 32 << 25 << 24;
+    parcours << 23 << 23;
 
     //Initialisation des aiguillages
-    diriger_aiguillage(8,  DEVIE,       0);
-    diriger_aiguillage(2,  DEVIE,       0);
-    diriger_aiguillage(20, DEVIE,       0);
-    diriger_aiguillage(14, DEVIE,       0);
+    diriger_aiguillage(1,  TOUT_DROIT,  0);
+    diriger_aiguillage(5,  TOUT_DROIT,  0);
+    diriger_aiguillage(7,  TOUT_DROIT,  0);
+    diriger_aiguillage(10, TOUT_DROIT,  0);
     diriger_aiguillage(11, TOUT_DROIT,  0);
+    diriger_aiguillage(13, TOUT_DROIT,  0);
+    diriger_aiguillage(16, TOUT_DROIT,  0);
     diriger_aiguillage(17, TOUT_DROIT,  0);
+    diriger_aiguillage(22, TOUT_DROIT,  0);
     diriger_aiguillage(23, TOUT_DROIT,  0);
 
     //Initialisation de la locomotive
-    locomotive.fixerNumero(1);
-    locomotive.fixerVitesse(12);
-    locomotive.fixerPosition(16, 23);
-    locomotive.allumerPhares();
-    locomotive.demarrer();
-    locomotive.afficherMessage("Ready!");
+    rose.fixerNumero(1);
+    rose.fixerVitesse(12);
+    rose.fixerPosition(16, 23);
+    rose.allumerPhares();
+    rose.demarrer();
+    rose.afficherMessage("Ready!");
 
-    //Attente du passage sur les contacts
-    for (int i = 1; i < parcours.size(); i++) {
-        attendre_contact(parcours.at(i));
-        afficher_message(qPrintable(QString("The engine no. %1 has reached contact no. %2.")
-                                    .arg(locomotive.numero()).arg(parcours.at(i))));
-        locomotive.afficherMessage(QString("I've reached contact no. %1.").arg(parcours.at(i)));
+    while(true){
+        //Attente du passage de la locomotive sur chacun des contacts du parcours
+        for(int tour = 1; tour <= 2; ++tour){
+             rose.afficherMessage(QString("Tour %1").arg(tour));
+            for (int cpt = 1; cpt < parcours.length(); cpt++) {
+                attendre_contact(parcours[cpt]);
+                rose.afficherMessage(QString("J'ai atteint le contact %1").arg(cpt));
+            }
+        }
+        rose.inverserSens();
+
     }
 
     //Arreter la locomotive
-    locomotive.arreter();
-    locomotive.afficherMessage("Yeah, piece of cake!");
+    rose.arreter();
+    rose.afficherMessage("Yeah, piece of cake!");
 
     //Fin de la simulation
     mettre_maquette_hors_service();
