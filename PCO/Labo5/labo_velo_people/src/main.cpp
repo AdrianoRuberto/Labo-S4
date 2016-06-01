601e86c49effff078727bf74573f76c375fc502b
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-06-01 10:38:46 +0200

    Van turns arround

--- PCO/Labo5/labo_velo_people/src/main.cpp
+++ PCO/Labo5/labo_velo_people/src/main.cpp
@@ -27,6 +27,33 @@ using namespace std;
 BikingInterface *gui_interface;
 
 #include <QThread>
+#include <QMutex>
+
+
+class Site {
+private:
+    int _nbBikes;
+    QMutex mutex;
+public:
+
+    const int MIN_BIKE;
+
+    Site(int nbBikes) : _nbBikes(nbBikes), MIN_BIKE(nbBikes){
+    }
+
+    int getBikes(){
+        mutex.lock();
+        size_t a = _nbBikes;
+        mutex.unlock();
+        return a;
+    }
+
+    void setBikes(int nbBikes) {
+        mutex.lock();
+        _nbBikes = nbBikes;
+        mutex.unlock();
+    }
+};
 
 /**
   Tâche illustrant les différents appels pouvant être faits à l'interface
@@ -34,8 +61,8 @@ BikingInterface *gui_interface;
   */
 class Habitant: public QThread
 {
-public:
-    Habitant(unsigned int id) : id(id) {}
+public:    
+    Habitant(unsigned int id, QVector<Site*>& sites) : id(id), _sites(&sites) {}
 
 void run() Q_DECL_OVERRIDE {
     unsigned int t = id;
@@ -50,8 +77,32 @@ void run() Q_DECL_OVERRIDE {
         gui_interface->consoleAppendText(t,"Salut");
         if (t==0) {
             // Déplacement de la camionnette
-            gui_interface->vanTravel(t,NBSITES,2000);
-            gui_interface->setBikes(NBSITES,((float)qrand())*5.0/RAND_MAX);
+            Site* site = _sites->at(NBSITES);
+            int a = min(2, site->getBikes());
+            if(a > MAX_BIKES)
+                a = MAX_BIKES;
+
+            site->setBikes(site->getBikes() - MAX_BIKES);
+
+            gui_interface->vanTravel(NBSITES, 0, 2000);
+            for(int i = 0; i < NBSITES; ++i){
+                Site* site = _sites->at(i);
+                int n = site->getBikes();
+                int minimum = site->MIN_BIKE;
+
+                if(n > minimum){
+                    size_t c = std::min(n - minimum, 4 - a);
+                    site->setBikes(n - c);
+                    gui_interface->setBikes(i, n - c);
+                    a += c;
+                } else {
+                    size_t c = std::min(minimum - n, a);
+                    site->setBikes(n + c);
+                    gui_interface->setBikes(i, n + c);
+                    a -= c;
+                }
+                gui_interface->vanTravel(i, i + 1, 2000);
+            }
         }
         else {
             // Déplacement d'un vélo
@@ -72,7 +123,12 @@ void run() Q_DECL_OVERRIDE {
 
 private:
     unsigned int id;
+
+    QVector<Site*>* _sites;
+
+    const size_t MAX_BIKES = 4;
 };
+
 
 int main(int argc, char *argv[])
 {
@@ -80,6 +136,7 @@ int main(int argc, char *argv[])
 
     int nbHabitants=NBHABITANTS;
     int nbSites=NBSITES;
+    int nbBornes = 4;
 
     // Initialisation de la partie graphique de l'application
     BikingInterface::initialize(nbHabitants,nbSites);
@@ -87,14 +144,22 @@ int main(int argc, char *argv[])
     gui_interface=new BikingInterface();
 
     // Création de threads
+    QVector<Site*> sites;
+    for(int i = 0; i <= nbSites; ++i){
+        sites.push_back(new Site(nbBornes - 2));
+        gui_interface->setBikes(i, nbBornes - 2);
+    }
+
+
     int NBTHREADS=nbHabitants;
     Habitant* threads[NBTHREADS];
     int t;
     for(t=0; t<NBTHREADS; t++){
         cout << "Création du thread "<< t << endl;
-        threads[t] = new Habitant(t);
+        threads[t] = new Habitant(t, sites);
         threads[t]->start();
     }
+
 
     // Attention, il est obligatoire d'exécuter l'instruction suivante.
     // C'est elle qui permet la gestion de la boucle des évévements de
